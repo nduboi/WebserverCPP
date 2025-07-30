5eb34373dd856c3eccebff0c90c1c4cef77ca8dd
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-07-30 22:10:54 +0200

    :sparkles: Adding port configuration using Toml

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include "server/HttpServer.hpp"
+#include "server/Utils.hpp"
 
 int main() {
     try {
         boost::asio::io_context io_context;
-        boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345));
+        unsigned int port = Utils::getConfigValue("port", 8080);
+        std::string static_root = Utils::getConfigValue<std::string>("static_root", "./www");
+        boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port));
 
-        std::cout << "Serveur TCP démarré. En attente de connexions..." << std::endl;
+        std::cout << "Serveur TCP démarré sur le port: "+ std::to_string(port) +". En attente de connexions..." << std::endl;
 
         for (;;) {
             boost::asio::ip::tcp::socket socket(io_context);
             acceptor.accept(socket);
-            HttpServer(std::move(socket));
+            HttpServer(std::move(socket), static_root);
         }
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
--- server/Utils.hpp
+++ server/Utils.hpp
@@ -9,12 +9,37 @@
 #define UTILS_HPP
 #include <string>
 #include <vector>
+#include <toml.hpp>
+#include <iostream>
 
 
 class Utils {
 public:
     static std::vector<std::string> splitSpace(std::string data);
     static std::string getFileContent(const std::string &filePath);
+    template <typename T>
+    static T getConfigValue(const std::string &key, const T &default_value, const std::string& path = "config.toml") {
+        try {
+            if (!std::filesystem::exists(path)) {
+                std::cerr << "[Config] Fichier " << path << " introuvable. Utilisation de la valeur par défaut.\n";
+                return default_value;
+            }
+
+            const auto data = toml::parse(path);
+
+            if (!data.contains(key)) {
+                std::cerr << "[Config] Clé \"" << key << "\" absente dans " << path << ". Valeur par défaut utilisée.\n";
+                return default_value;
+            }
+
+            return toml::find<T>(data, key);
+        } catch (const toml::type_error& e) {
+            std::cerr << "[Config] Erreur de type pour \"" << key << "\": " << e.what() << "\n";
+        } catch (const std::exception& e) {
+            std::cerr << "[Config] Erreur lors de la lecture de \"" << path << "\": " << e.what() << "\n";
+        }
+        return default_value;
+    }
 };
 
 
